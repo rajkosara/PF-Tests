e35b3fde6b1673b0677b08add0ece1f35688b342
David Chambers <daa.chambers@gmail.com>
2013-03-04 12:53:14 +0000

    changed hooks

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -8,13 +8,13 @@ end
 
 Before do
       Timeout.timeout(30) { sleep(0.5) while Capybara.current_url != "about:blank"}
- # if ENV['JUNIT_OUTPUT_SMOKE'] == "/Users/Test/.jenkins/jobs/Run_Smoke_Tests/workspace"
-  #  sleep 2
-  #end
+  if ENV['JUNIT_OUTPUT_SMOKE'] == "/Users/Test/.jenkins/jobs/Run_Smoke_Tests/workspace"
+    sleep 2
+  end
 end
 
 After do
-  #if ENV['JUNIT_OUTPUT_SMOKE'] == "/Users/Test/.jenkins/jobs/Run_Smoke_Tests/workspace"
-  #  sleep 2
-  #end
+  if ENV['JUNIT_OUTPUT_SMOKE'] == "/Users/Test/.jenkins/jobs/Run_Smoke_Tests/workspace"
+    sleep 2
+  end
 end
