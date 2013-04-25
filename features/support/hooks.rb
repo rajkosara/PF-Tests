0d24bd70f0785ba9c31466e08972e5aecefb4064
David Chambers <daa.chambers@gmail.com>
2013-04-25 14:58:19 +0100

    removed sleep

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -9,11 +9,9 @@ end
 Before do
   #page.execute_script "window.open();"
   Capybara.reset!#_session!
-  sleep 1
 end
 
 After do
   page.driver.browser.quit #.execute_script "window.close();"
   Capybara.send(:session_pool).delete_if { |key, value| key =~ /selenium/i }
-  sleep 1
 end
